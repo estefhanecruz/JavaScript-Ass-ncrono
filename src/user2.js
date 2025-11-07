ec1d6062eab74c618438fc2872fb36a8658d0240
Julia Melo <estefhanecruz@gmail.com>
2025-11-07 19:02:37 -0300

    Callback, callback hell e promises

---
+++ src/user2.js
@@ -0,0 +1,35 @@
+function obterClienteApi(){
+    return new Promise ((resolve, reject)=>{
+        setTimeout(()=>{
+            console.log("Obter cliente na API");
+
+            resolve({
+                id: 1,
+                nome: "Julia",
+                idade: 25,
+            });
+        },2000);
+    })
+    }
+function tratarClienteApi(cliente){
+    return new Promise ((resolve, reject)=>{
+        setTimeout(()=>{
+            console.log("tratar dados do cliente API");
+            if (!cliente.nome){
+                reject("Nome do cliente ausente");
+            }
+            cliente.nome = cliente.nome.toUpperCase();
+            resolve(cliente);
+        }, 2000);
+    });
+
+}
+
+obterClienteApi()
+    .then(tratarClienteApi)
+    .then((clienteTratado)=>{
+        console.log(clienteTratado);
+    })
+    .catch((error)=>{
+        console.log(error);
+    });
